8045b3f328c240b4f3f611383ea2d6cb651385af
Amit Yadav <akydv2627@gmail.com>
2025-11-12 01:26:52 +0530

    Add HeaderPropagation with every client Request

---
+++ src/main/java/com/bookticket/movie_service/configuration/HeaderPropagationInterceptor.java
@@ -0,0 +1,34 @@
+package com.bookticket.movie_service.configuration;
+
+import org.springframework.http.HttpRequest;
+import org.springframework.http.client.ClientHttpRequestExecution;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import java.io.IOException;
+
+public class HeaderPropagationInterceptor implements ClientHttpRequestInterceptor {
+
+    @Override
+    public ClientHttpResponse intercept(HttpRequest request, byte[] body, ClientHttpRequestExecution execution) throws IOException {
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        
+        if (attributes != null) {
+            String userId = attributes.getRequest().getHeader("X-User-Id");
+            String userRoles = attributes.getRequest().getHeader("X-User-Roles");
+            
+            if (userId != null) {
+                request.getHeaders().add("X-User-Id", userId);
+            }
+            
+            if (userRoles != null) {
+                request.getHeaders().add("X-User-Roles", userRoles);
+            }
+        }
+        
+        return execution.execute(request, body);
+    }
+}
+
